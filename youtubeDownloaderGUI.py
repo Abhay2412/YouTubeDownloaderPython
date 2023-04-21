4f7ecaf2c91d97192759f133c1def57e63283cec
Abhay2412 <abhaykhosla0@gmail.com>
2023-04-21 17:05:47 -0600

    Add the download function with a progress message displaying

--- youtubeDownloaderGUI.py
+++ youtubeDownloaderGUI.py
@@ -15,8 +15,25 @@ application.title("YouTube Video Downloader")
 title = customtkinter.CTkLabel(application, text="Paste a YouTube video link")
 title.pack(padx=15, pady=15)
 
+def startDownload():
+    try:
+        linkToDownload = youtubeLink.get()
+        youTubeVideoObject = YouTube(linkToDownload)
+        video = youTubeVideoObject.streams.get_highest_resolution()
+        video.download()
+        videoTitle = youTubeVideoObject.title
+    except:
+        print("YouTube Link is Invalid")
+    statusMessageText = customtkinter.CTkLabel(application, text="The video:" + videoTitle + " is downloaded!")
+    statusMessageText.pack(padx=25, pady=25)
+
 # Textbox input
-youtubeLink = customtkinter.CTkEntry(application, width=400, height=50)
+youtubeURL = tkinter.StringVar()
+youtubeLink = customtkinter.CTkEntry(application, width=400, height=50, textvariable=youtubeURL)
 youtubeLink.pack()
+
+# Download Button 
+downloadButton = customtkinter.CTkButton(application, text="Download", command=startDownload)
+downloadButton.pack(padx=15, pady=15)
 
 application.mainloop()
