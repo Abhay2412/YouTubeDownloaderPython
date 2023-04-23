f6f8804bae8fe27a2ab0156ef1acaecd3a40f3b5
Abhay2412 <abhaykhosla0@gmail.com>
2023-04-22 18:25:55 -0600

    Add in progress percentage and bar

--- youtubeDownloaderGUI.py
+++ youtubeDownloaderGUI.py
@@ -18,19 +18,40 @@ title.pack(padx=15, pady=15)
 def startDownload():
     try:
         linkToDownload = youtubeLink.get()
-        youTubeVideoObject = YouTube(linkToDownload)
+        youTubeVideoObject = YouTube(linkToDownload, on_progress_callback=inProgress)
         video = youTubeVideoObject.streams.get_highest_resolution()
         video.download()
         videoTitle = youTubeVideoObject.title
+        statusMessageText = customtkinter.CTkLabel(application, text="The video:" + videoTitle + " is downloaded!", text_color="green") 
+        statusMessageText.pack(padx=25, pady=25)
     except:
-        print("YouTube Link is Invalid")
-    statusMessageText = customtkinter.CTkLabel(application, text="The video:" + videoTitle + " is downloaded!")
-    statusMessageText.pack(padx=25, pady=25)
+        statusMessageText = customtkinter.CTkLabel(application, text="The link:" + linkToDownload + " is invalid!", text_color="red")
+        statusMessageText.pack(padx=25, pady=25)
+
+def inProgress(stream, chunk, bytes_remaining):
+    totalSize = stream.filesize
+    bytesDownloaded = totalSize - bytes_remaining
+    percentageOfCompletion = bytesDownloaded / totalSize * 100
+    percentageOfCompletion = str(int(percentageOfCompletion))
+    # Update Percentage
+    progressPercentage.configure(text=percentageOfCompletion + "%")
+    progressPercentage.update()
+    #Update Progress Bar
+    progressBar.set(float(percentageOfCompletion) / 100)
 
 # Textbox input
 youtubeURL = tkinter.StringVar()
 youtubeLink = customtkinter.CTkEntry(application, width=400, height=50, textvariable=youtubeURL)
 youtubeLink.pack()
+
+# Progress Percentage 
+progressPercentage = customtkinter.CTkLabel(application, text="0%", text_color="pink")
+progressPercentage.pack()
+
+# Progress Bar
+progressBar = customtkinter.CTkLael(application, )
+progressBar.set(0)
+progressBar.pack(padx=15, pady=15)
 
 # Download Button 
 downloadButton = customtkinter.CTkButton(application, text="Download", command=startDownload)
